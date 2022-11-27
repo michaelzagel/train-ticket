f5ed57f89bcb247dcda7bf77d311f74a581a9390
ofirabu42 <ofir@backslash.security>
2022-11-27 15:52:55 +0200

    feat: added dummy call for flow purose

--- ts-gateway-service/src/main/java/gateway/GatewayApplication.java
+++ ts-gateway-service/src/main/java/gateway/GatewayApplication.java
@@ -15,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+        httpPost("/MovieService/findMovie");
     }
 
 }
